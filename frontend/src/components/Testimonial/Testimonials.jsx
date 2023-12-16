9e78f76574860fbaa915f833b37350a24b13ff8b
iaabrar16 <kingaabrar@gmail.com>
2023-12-16 16:02:52 +0600

    Travel Mern Project Done

---
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import Slider from 'react-slick'
+import Aabrar from '../../assets/images/Aabrar.jpg'
+import Nirob from '../../assets/images/Nirob.jpg'
+import shemin from '../../assets/images/shemin.jpg'
+import shuvo from '../../assets/images/shuvo.jpg'
+import payel from '../../assets/images/payel.jpg'
+
+
+const Testimonials = () => {
+   const settings = {
+      dots:true,
+      infinite:true,
+      autoplay:true,
+      speed:1000,
+      swipeToSlide:true,
+      autoplaySpeed:2000,
+      slidesToShow:3,
+
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+               infinite: true,
+               dots: true,
+            },
+            // eslint-disable-next-line no-dupe-keys
+            breakpoint: 576,
+            // eslint-disable-next-line no-dupe-keys
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               infinite: true,
+               dots: true,
+            },
+         }
+      ]
+   }
+
+   return <Slider {...settings}>
+      <div className="testimonial py-4 px-3">
+         <p>Travel World has transformed the way I see the world. Their attention to detail and personalized service made my trip unforgettable. I can't wait for my next adventure with them!
+         </p>
+
+         <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={Aabrar} className='w-25 h-25 rounded-2' alt="" />
+            <div>
+               <h6 className='mb-0 mt-3'>Ismail Aabrar</h6>
+               <p>Customer</p>
+            </div>
+         </div> 
+      </div>
+
+      <div className="testimonial py-4 px-3">
+         <p>Travel World's tours are simply amazing. They take care of everything, allowing me to fully immerse myself in the experience. Highly recommended!
+         </p>
+
+         <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={Nirob} className='w-25 h-25 rounded-2' alt="" />
+            <div>
+               <h6 className='mb-0 mt-3'>Shazid Nirob</h6>
+               <p>Customer</p>
+            </div>
+         </div> 
+      </div>
+
+      <div className="testimonial py-4 px-3">
+         <p>I've been on three tours with Travel World and each one was unique and exciting. Their guides are knowledgeable and friendly. Can't wait for my next trip!
+         </p>
+
+         <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={shemin} className='w-25 h-25 rounded-2' alt="" />
+            <div>
+               <h6 className='mb-0 mt-3'>Shemin Mirza</h6>
+               <p>Customer</p>
+            </div>
+         </div> 
+      </div>
+
+      <div className="testimonial py-4 px-3">
+         <p>I've been on three tours with Travel World and each one was unique and exciting. Their guides are knowledgeable and friendly. Can't wait for my next trip!
+         </p>
+
+         <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={shuvo} className='w-25 h-25 rounded-2' alt="" />
+            <div>
+               <h6 className='mb-0 mt-3'>Kazi Farid Shuvo</h6>
+               <p>Customer</p>
+            </div>
+         </div> 
+      </div>
+
+      <div className="testimonial py-4 px-3">
+         <p>I've been on three tours with Travel World and each one was unique and exciting. Their guides are knowledgeable and friendly. Can't wait for my next trip!
+         </p>
+
+         <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={payel} className='w-25 h-25 rounded-2' alt="" />
+            <div>
+               <h6 className='mb-0 mt-3'>Mozammel Haque Payel</h6>
+               <p>Customer</p>
+            </div>
+         </div> 
+      </div>
+   </Slider>
+}
+
+export default Testimonials
---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ServiceCard from './ServiceCard'
+import { Col } from 'reactstrap'
+import weatherImg from '../assets/images/weather.png'
+import guideImg from '../assets/images/guide.png'
+import customizationImg from '../assets/images/customization.png'
+
+const servicesData = [
+   {
+      imgUrl: weatherImg,
+      title: `Calculate Weather`,
+      desc: `Accurate Weather Forecast.
+       Get the most precise weather forecasts and updates to plan your day effectively.`,
+   },
+   {
+      imgUrl: guideImg,
+      title: `Best Tour Guide`,
+      desc: `Expert Guided Tours.
+      Explore amazing destinations with our experienced tour guides who provide in-depth knowledge.`,
+   },
+   {
+      imgUrl: customizationImg,
+      title: 'Customization',
+      desc: `Tailored Solutions.
+      We create personalized solutions to meet your unique needs and preferences, ensuring your satisfaction.`,
+   },
+]
+
+const ServiceList = () => {
+   return <>
+      {
+         servicesData.map((item, index) => (
+            <Col lg='3' md='6' sm='12' className='mb-4' key={index}>
+               <ServiceCard item={item} />
+            </Col>))
+      }
+   </>
+
+}
+
+export default ServiceList
