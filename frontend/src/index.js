8ab285068b275166de79ca73fe31a9cb88956507
Kazi Farid Shuvo <115430342+Kazi-Farid-Shuvo@users.noreply.github.com>
2023-12-22 00:54:37 +0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,14 @@ import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
