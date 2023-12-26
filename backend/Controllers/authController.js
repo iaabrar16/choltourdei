a4ab95d1f24c33f82f2f9661260b3783dc76b44b
Shazid <shossain201271@bscse.uiu.ac.bd>
2023-12-26 21:50:44 +0600

    5th updatd

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -18,7 +18,7 @@ export const register = async (req, res) => {
 
       await newUser.save()
 
-      res.status(200).json({ success: true, message: "Successfully created!" })
+      res.status(200).json({ success: true, message: " created Successfully!" })
    } catch (error) {
       res.status(500).json({ success: false, message: "Failed to create! Try again." })
    }
