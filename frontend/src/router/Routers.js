106a0f282260b1c92f2668118f198394ce310757
Shazid <shossain201271@bscse.uiu.ac.bd>
2023-12-27 21:10:04 +0600

    8th update

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import TourDetails from './../pages/TourDetails'
+import Tours from './../pages/Tours'
+import Register from './../pages/Register'
+import SearchResultList from './../pages/SearchResultList'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import ThankYou from '../pages/ThankYou'
 import Home from './../pages/Home'
 import Login from './../pages/Login'
-import Register from './../pages/Register'
-import SearchResultList from './../pages/SearchResultList'
-import TourDetails from './../pages/TourDetails'
-import Tours from './../pages/Tours'
+
+
 
 const Routers = () => {
    return (
