6a1aea91542c140b36a1b17a5c31e5faad4f5684
iaabrar16 <kingaabrar@gmail.com>
2024-01-01 19:36:49 +0600

    review.js updated

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -4,7 +4,7 @@ const reviewSchema = new mongoose.Schema(
   {
     productId: {
       type: mongoose.Types.ObjectId,
-      ref: "Tour",
+      ref: "Tours",
     },
     username: {
       type: String,
@@ -25,4 +25,4 @@ const reviewSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Review", reviewSchema);
+export default mongoose.model("Reviews", reviewSchema);
